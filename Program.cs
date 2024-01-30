75bb03d9a9e3ab3dbdb700571598dae0edc0c029
conormendenhall <conor.mendenhall@gmail.com>
2024-01-30 09:18:14 -0500

    spectre.console for enhanced experience

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
-﻿public class Program
+﻿using Spectre.Console;
+
+public class Program
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, Warrior!");
-        Console.WriteLine("What is your name?");
 
-        var warriorName = Console.ReadLine();
+        var warriorName = AnsiConsole.Ask<string>("What is your [red]name[/]?");
         var hero = new Hero(warriorName);
 
-        Console.WriteLine($"Well met, {hero.Name}.");
+        AnsiConsole.Markup($"Well met, [red]{hero.Name}[/].\n");
 
         Random random = new Random();
 
@@ -17,21 +18,21 @@
             if (hero.HP < hero.MaxHP)
             {
                 var restHP = random.Next(1, 3);
-                Console.WriteLine($"You rest and restore {restHP} hit points.");
 
                 if (hero.HP + restHP <= hero.MaxHP)
                 {
                     hero.HP += restHP;
+                    Console.WriteLine($"You rest and restore {restHP} HP.");
                 }
                 else
                 {
                     hero.HP = 5;
+                    Console.WriteLine($"You rest and feel fully restored.");
                 }
-
-                Console.WriteLine($"[hero has {hero.HP} HP]");
+                Console.WriteLine($"You have {hero.HP} HP.\n");
             }
 
-            Console.WriteLine("You encounter a Goblin.");
+            AnsiConsole.Markup($"You encounter a [chartreuse3]goblin[/].");
             var goblin = new Goblin(hp: 5);
 
             while (goblin.HP > 0 && hero.HP > 0)
@@ -43,41 +44,40 @@
                 Console.WriteLine($"You deal {atkDmg} damage.");
                 goblin.HP -= atkDmg;
 
-                Console.WriteLine($"[goblin has {goblin.HP} HP]");
-
                 if (goblin.HP <= 0)
                 {
-                    Console.WriteLine("The goblin falls dead at your feet.");
-                    Console.WriteLine($"{hero.Name} stands victorious!");
+                    AnsiConsole.Markup($"The [chartreuse3]goblin[/] falls dead at your feet.\n");
+                    Console.WriteLine($"{hero.Name} stands victorious!\n");
                     hero.FelledFoes += 1;
 
                     var loot = random.Next(5);
-                    Console.WriteLine($"You loot the goblin for {loot} gold pieces. You drop them into your coinpurse.");
+                    AnsiConsole.Markup($"You loot the [chartreuse3]goblin[/] for {loot} gold pieces. You drop them into your coinpurse.\n");
                     hero.Gold += loot;
-                    Console.WriteLine($"[hero has {hero.Gold} gp]");
+                    Console.WriteLine($"You are carrying {hero.Gold} gold.\n");
                 }
                 else
                 {
-                    Console.WriteLine("The goblin still stands, sneering at you.");
-                    Console.WriteLine("The goblin attacks!");
+                    AnsiConsole.Markup("The [chartreuse3]goblin[/] still stands, sneering at you.\n");
+                    Console.WriteLine();
 
+                    AnsiConsole.Markup("The [chartreuse3]goblin[/] attacks!\n");
                     var gobAtkDmg = random.Next(5);
 
-                    Console.WriteLine($"The goblin deals {gobAtkDmg} damage.");
+                    AnsiConsole.Markup($"The [chartreuse3]goblin[/] deals {gobAtkDmg} damage.\n");
                     hero.HP -= gobAtkDmg;
 
                     Console.WriteLine($"[hero has {hero.HP} HP]");
 
                     if (hero.HP <= 0)
                     {
-                        Console.WriteLine("The goblin strikes you down.");
+                        AnsiConsole.Markup("The [chartreuse3]goblin[/] strikes you down.\n");
                         Console.WriteLine($"{hero.Gold} gold pieces spill out of your coinpurse.");
                         Console.WriteLine($"You felled {hero.FelledFoes} foes before meeting your end.");
                         Console.WriteLine($"Rest in peace, {hero.Name}.");
                     }
                     else
                     {
-                        Console.WriteLine("You are hurt but not dead yet. You steel your nerves for another attack.");
+                        Console.WriteLine($"You are hurt but not dead yet. You steel your nerves for another attack.{Environment.NewLine}");
                     }
                 }
             }
